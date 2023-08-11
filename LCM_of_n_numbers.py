d056f71435eea43ab66df7d000bb09f2abdaea78
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2023-08-11 21:01:52 +0530

    n=int(input()) a=list(map(int,input().split())) i=1 while(i>0):     c=0     for j in a:         if i%j==0:             c+=1     if c==n:         print(i)         break     else:         i+=1

---
+++ LCM_of_n_numbers.py
@@ -0,0 +1,13 @@
+n=int(input())
+a=list(map(int,input().split()))
+i=1
+while(i>0):
+    c=0
+    for j in a:
+        if i%j==0:
+            c+=1
+    if c==n:
+        print(i)
+        break
+    else:
+        i+=1
